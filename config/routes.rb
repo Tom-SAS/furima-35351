173d21a035d4e2b1ed8eeed2ef52955e934f2675
Tom-SAS <tommakuhari@gmail.com>
2021-06-26 14:51:53 +0900

    商品詳細表示機能１

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   
   root to: 'products#index'
-  resources :products, only:[:new, :create]
+  resources :products, only:[:new, :create, :show]
   # resources :products, only:[:new, :create] do
   #   resources :user_products, only:[:new]
   # end
