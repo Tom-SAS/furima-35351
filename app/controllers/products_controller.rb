0bc243312e33cb5097c77454fe3498e92ee0f995
Tom-SAS <tommakuhari@gmail.com>
2021-06-26 20:21:25 +0900

    商品編集機能１

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,7 @@
 class ProductsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit]
+  before_action :set_product, only: [:show, :edit, :update, :move_to_index]
+  before_action :move_to_index, except: [:index, :new]
 
   def index
     @products = Product.order('created_at DESC')
@@ -19,7 +21,18 @@ class ProductsController < ApplicationController
   end
 
   def show
-    @product = Product.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    @product.update(product_params)
+    if @product.save
+      redirect_to product_path(@product.id)
+    else
+      render :edit
+    end
   end
 
   private
@@ -27,5 +40,14 @@ class ProductsController < ApplicationController
   def product_params
     params.require(:product).permit(:name, :image, :comment, :category_id, :status_id, :shipment_cost_id, :prefecture_id,
                                     :delivery_time_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  def move_to_index
+    @product = set_product
+    redirect_to action: :index unless current_user.id == @product.user.id
   end
 end
